6a6e2bc46417804433d4019dbe0a57c26cf97558
Brian Green <greenbrian@gmail.com>
2017-02-10 13:20:26 +0000

    dnsmasq config

--- packer/scripts/dnsmasq.sh
+++ packer/scripts/dnsmasq.sh
@@ -3,4 +3,7 @@
 echo "Installing dnsmqasq..."
 sudo apt-get install -y -q dnsmasq
 
-echo "server=/consul/127.0.0.1#8600" > /etc/dnsmasq.d/10-consul
+echo "Configuring DNSmasq"
+sudo bash -c "cat >/etc/dnsmasq.d/10-consul" << EOF
+server=/consul/127.0.0.1#8600
+EOF

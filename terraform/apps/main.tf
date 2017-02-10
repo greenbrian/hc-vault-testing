02a111814d446fe850f0f8318906f94043613fef
Brian Green <greenbrian@gmail.com>
2017-02-10 14:12:55 +0000

    trying to get atlas remote state working with outputs

--- terraform/apps/main.tf
+++ terraform/apps/main.tf
@@ -7,6 +7,7 @@ data "terraform_remote_state" "hcvt_consul_vault" {
   backend = "atlas"
   config {
     name = "bgreen/hcvt_consul_vault"
+    access_token = "$"
   }
 }
 
@@ -14,9 +15,9 @@ module "haproxy" {
   source         = "./haproxy"
   user           = "${var.user}"
   key_path       = "${var.bg_priv_key}"
-  primary_consul = "${terraform_remote_state.hcvt_consul_vault.primary_consul}"
-  subnet_id      = "${terraform_remote_state.hcvt_consul_vault.subnet_id}"
-  hcvt_sg_id     = "${terraform_remote_state.hcvt_consul_vault.hcvt_sg_id}"
+  primary_consul = "${terraform_remote_state.hcvt_consul_vault.output.primary_consul}"
+  subnet_id      = "${terraform_remote_state.hcvt_consul_vault.output.subnet_id}"
+  hcvt_sg_id     = "${terraform_remote_state.hcvt_consul_vault.output.hcvt_sg_id}"
 }
 
 module "nginx" {
@@ -24,7 +25,7 @@ module "nginx" {
   user               = "${var.user}"
   priv_key           = "${var.bg_priv_key}"
   nginx_server_count = 2
-  primary_consul     = "${terraform_remote_state.hcvt_consul_vault.primary_consul}"
-  subnet_id          = "${terraform_remote_state.hcvt_consul_vault.subnet_id}"
-  hcvt_sg_id         = "${terraform_remote_state.hcvt_consul_vault.hcvt_sg_id}"
+  primary_consul     = "${terraform_remote_state.hcvt_consul_vault.output.primary_consul}"
+  subnet_id          = "${terraform_remote_state.hcvt_consul_vault.output.subnet_id}"
+  hcvt_sg_id         = "${terraform_remote_state.hcvt_consul_vault.output.hcvt_sg_id}"
 }

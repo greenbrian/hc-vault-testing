c52ea873ab0f3886f699f8032fc188ee78107e8f
greenbrian <greenbrian@gmail.com>
2017-07-27 18:32:43 -0500

    adding openssl check in output

--- terraform/apps/nginx/outputs.tf
+++ terraform/apps/nginx/outputs.tf
@@ -1,3 +1,7 @@
 output "nginx_addresses" {
   value = "${formatlist("ssh://%s", aws_instance.nginx.*.public_dns)}"
 }
+
+output "nginx_cert_check" {
+  value = "${formatlist("echo | openssl s_client -showcerts -servername foo.example.com -connect %s:443 2>/dev/null | openssl x509 -inform pem -noout -text | head -n 14", aws_instance.nginx.*.public_dns)}"
+}
